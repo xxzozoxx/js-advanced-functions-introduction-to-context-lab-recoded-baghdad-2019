21fbebe63589361a8a5817aa4fd8f28415043ffc
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-11 15:43:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function createEmployeeRecord(arr) {
+    let employeeRecord = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord;
+}
+function createEmployees (arr){
+
+}
